6a416741b7c467b580910dcb01bcf8cb44acb3f2
3n1gmus <31117809+3n1gmus@users.noreply.github.com>
2024-07-08 08:44:11 -0600

    init

---
+++ Printers_Drivers.sh
@@ -0,0 +1,13 @@
+# --- Setup for HL-L3210CW ---
+# https://aur.archlinux.org/packages/brother-hl-l3210cw
+# http://<IP>:631/ipp
+
+# Yay install method
+# yay -S brother-hl-l3210cw
+
+# Manual Git Method
+# From: https://linux-packages.com/aur/package/brother-hl-l3210cw
+# sudo pacman -S --needed base-devel git
+# git clone https://aur.archlinux.org/brother-hl-l3210cw.git ~/brother-hl-l3210cw
+# cd ~/brother-hl-l3210cw
+# makepkg -si
